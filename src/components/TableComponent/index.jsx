49acff94ccab54fd07389e33ae3a674446873e19
sabilimaulana <spacysbl.learn@gmail.com>
2021-08-27 20:04:41 +0700

    Search and pagination feature

--- src/components/TableComponent/index.jsx
+++ src/components/TableComponent/index.jsx
@@ -2,11 +2,15 @@ import { faEdit, faInfo, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import BootstrapTable from "react-bootstrap-table-next";
 import { Button, Container } from "reactstrap";
+import ToolkitProvider, { Search } from "react-bootstrap-table2-toolkit";
+import paginationFactory from "react-bootstrap-table2-paginator";
 
+const { SearchBar } = Search;
 const columns = [
   {
     dataField: "id",
     text: "ID",
+    sort: true,
     headerStyle: () => {
       return { width: "5%" };
     },
@@ -14,10 +18,12 @@ const columns = [
   {
     dataField: "nama",
     text: "Nama",
+    sort: true,
   },
   {
     dataField: "alamat",
     text: "Alamat",
+    sort: true,
   },
   {
     dataField: "link",
@@ -25,13 +31,13 @@ const columns = [
     formatter: (rowContent, row) => {
       return (
         <div>
-          <Button color="dark" className="me-4">
+          <Button color="dark" className="me-2">
             <FontAwesomeIcon icon={faInfo} /> Detail
           </Button>
-          <Button color="dark" className="me-4">
+          <Button color="dark" className="me-2">
             <FontAwesomeIcon icon={faEdit} /> Edit
           </Button>
-          <Button color="dark" className="me-4">
+          <Button color="dark" className="me-2">
             <FontAwesomeIcon icon={faTrash} /> Delete
           </Button>
         </div>
@@ -40,10 +46,35 @@ const columns = [
   },
 ];
 
+const defaultSorted = [
+  {
+    dataField: "id", // if dataField is not match to any column you defined, it will be ignored.
+    order: "asc", // desc or asc
+  },
+];
+
 const TableComponent = ({ users }) => {
   return (
     <Container>
-      <BootstrapTable keyField="id" data={users} columns={columns} />
+      <ToolkitProvider
+        keyField="id"
+        data={users}
+        columns={columns}
+        defaultSorted={defaultSorted}
+        search
+      >
+        {(props) => (
+          <div>
+            <div className="float-end mb-2">
+              <SearchBar placeholder="Search ..." {...props.searchProps} />
+            </div>
+            <BootstrapTable
+              {...props.baseProps}
+              pagination={paginationFactory()}
+            />
+          </div>
+        )}
+      </ToolkitProvider>
     </Container>
   );
 };
